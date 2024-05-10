33e6c0a1bf714c7186069b0f2d21d4ee4b2b4e66
Rivaldolim <126888076+Rivaldolim@users.noreply.github.com>
2024-05-10 23:28:11 +0800

    Create LOGIN.php

---
+++ LOGIN.php
@@ -0,0 +1,85 @@
+<?php
+    require "login_process.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Sign Up</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap"
+    rel="stylesheet">
+
+  <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+  <link rel="stylesheet" href="css/animate.css">
+
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/magnific-popup.css">
+
+  <link rel="stylesheet" href="css/aos.css">
+
+  <link rel="stylesheet" href="css/ionicons.min.css">
+
+  <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+  <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+  <link rel="stylesheet" href="css/flaticon.css">
+  <link rel="stylesheet" href="css/icomoon.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <!-- Registration 13 - Bootstrap Brain Component -->
+  <section style="height: 100vh;" class="bg-dark py-3 py-md-5">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+          <div style="color:white; background-color:black" class="card border border-light-subtle rounded-3 shadow-sm">
+            <div class="card-body p-3 p-md-4 p-xl-5">
+              <div class="text-center mb-3">
+                <a style="color:white;" class="navbar-brand" href="index.php">Fumeo<span
+                    style="color:#01d28e;">Showroom</span></a>
+              </div>
+              <h2 class="fs-6 fw-normal text-center text-secondary mb-4">Enter your details to register</h2>
+              <form action="login_process.php" method="POST">
+                <div class="row gy-2 overflow-hidden">
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input type="text" class="form-control" name="username" id="username" placeholder="Username"
+                        required>
+                      <label for="username" class="form-label">Username</label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input type="password" class="form-control" name="password" id="password" value=""
+                        placeholder="Password" required>
+                      <label for="password" class="form-label">Password</label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="d-grid my-3">
+                      <button class="btn btn-primary btn-lg" type="submit">Login</button>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <p class="m-0 text-secondary text-center">Doesn't have an account? <a href="signup.php"
+                        class="link-primary text-decoration-none">Sign up</a></p>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</body>
+
+</html>
